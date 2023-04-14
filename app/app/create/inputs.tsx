7f06fb719c419c50b46c83e685bac570333d740b
clearloop <26088946+clearloop@users.noreply.github.com>
2023-04-15 05:35:39 +0900

    feat(app): make next.js happy (#18)

--- app/app/create/inputs.tsx
+++ app/app/create/inputs.tsx
@@ -6,6 +6,7 @@ import { Listbox, Transition } from "@headlessui/react";
 import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
 import { ICollection } from "@/interfaces";
 import { useState, useEffect, Fragment } from "react";
+import Image from "next/image";
 
 export function CountInput({
     count,
@@ -119,10 +120,12 @@ export function CollectionInput({
                     <div className="relative mt-2">
                         <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
                             <span className="flex items-center">
-                                <img
+                                <Image
                                     src={selected.logo}
                                     alt=""
-                                    className="h-5 w-5 flex-shrink-0 rounded-full"
+                                    width={20}
+                                    height={20}
+                                    className="flex-shrink-0 rounded-full"
                                 />
                                 <span className="ml-3 block truncate">{selected.name}</span>
                             </span>
@@ -156,9 +159,11 @@ export function CollectionInput({
                                         {({ selected, active }) => (
                                             <>
                                                 <div className="flex items-center">
-                                                    <img
+                                                    <Image
                                                         src={collection.logo}
                                                         alt=""
+                                                        width={20}
+                                                        height={20}
                                                         className="h-5 w-5 flex-shrink-0 rounded-full"
                                                     />
                                                     <span
--- app/next.config.js
+++ app/next.config.js
@@ -3,6 +3,9 @@ const nextConfig = {
   experimental: {
     appDir: true,
   },
+  images: {
+    domains: ['logo.nftscan.com'],
+  },
 }
 
 module.exports = nextConfig
