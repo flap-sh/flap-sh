907d406595609d0e88b6e993f43d7ae968ae4bb3
clearloop <26088946+clearloop@users.noreply.github.com>
2023-04-23 15:40:30 +0800

    feat(app): filter pool list by pool state (#42)
    * feat(app): filter pools by pool state
    
    * feat(app): footer at bottom

---
+++ app/app/index/dropdown.tsx
@@ -0,0 +1,46 @@
+"use cilent"
+import { STATES } from '@/hooks/usePool'
+import { Menu, Transition } from '@headlessui/react'
+import { Fragment, useState } from 'react'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSortDown } from "@fortawesome/free-solid-svg-icons";
+
+export function Dropdown({ filterState }: { filterState: (state: number) => void }) {
+    const [state, setState] = useState(0);
+
+    return (
+        <Menu>
+            <Menu.Button>
+                <span className="flex flex-row items-center">
+                    <span>{STATES[state].toLocaleLowerCase()}</span>
+                    <FontAwesomeIcon
+                        icon={faSortDown}
+                        className="pl-1 pb-1 hover:cursor-pointer"
+                    />
+                </span>
+            </Menu.Button>
+            <Transition
+                as={Fragment}
+                enter="transition ease-out duration-100"
+                enterFrom="transform opacity-0 scale-95"
+                enterTo="transform opacity-100 scale-100"
+                leave="transition ease-in duration-75"
+                leaveFrom="transform opacity-100 scale-100"
+                leaveTo="transform opacity-0 scale-95"
+            >
+                <Menu.Items className="absolute rounded-sm bg-black flex flex-col border border-solid border-gray-500 p-2 opacity-90 focus:outline-none">
+                    {STATES.map((state, index) => (
+                        <Menu.Item key={index}>
+                            <span onClick={() => {
+                                setState(index)
+                                filterState(index)
+                            }}>
+                                {state.toLocaleLowerCase()}
+                            </span>
+                        </Menu.Item>
+                    ))}
+                </Menu.Items>
+            </Transition>
+        </Menu>
+    )
+}
